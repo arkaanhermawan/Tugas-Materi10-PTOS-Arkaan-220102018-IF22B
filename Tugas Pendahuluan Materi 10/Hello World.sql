c1c3da323ec41bc9b9863c415079219ee78e455f
arkaanhermawan <86821879+arkaanhermawan@users.noreply.github.com>
2023-12-05 16:36:17 +0700

    Add files via upload

---
+++ Tugas Pendahuluan Materi 10/Hello World.sql
@@ -0,0 +1,8 @@
+-- Membuat tabel
+CREATE TABLE HelloWorld (
+    id INT PRIMARY KEY,
+    message VARCHAR(255)
+);
+
+-- Menambahkan data
+INSERT INTO HelloWorld (id, message) VALUES (1, 'Hello, World!');
